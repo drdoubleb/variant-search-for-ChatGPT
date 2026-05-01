be88157b02d9202b98bc937995834c27097253bf
drdoubleb <114887827+drdoubleb@users.noreply.github.com>
2026-05-01 16:51:36 -0400

    Update tp53.js

--- api/tp53.js
+++ api/tp53.js
@@ -270,10 +270,11 @@ function buildMatches(rows, { protein, cdna, genomic }) {
 }
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     return res.status(204).end();
   }
 
